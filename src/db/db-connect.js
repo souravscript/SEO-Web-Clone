340c74d3eeeae22fdbc88f21f3eebac1505346a7
souravscript <souravspace@Shivams-MacBook-Air.local>
2024-12-25 00:49:50 +0530

    MONGO_URI moved to .env

--- src/db/db-connect.js
+++ src/db/db-connect.js
@@ -5,7 +5,7 @@ const connectToDatabase = async () => {
     // If already connected, return the existing connection
     return;
   }
-  const MONGO_URI="mongodb+srv://souravshivam1857:0EsTxXMeftNw6gCw@cluster0.jehif.mongodb.net/SEO_Engine?retryWrites=true&w=majority&appName=Cluster0"
+  const MONGO_URI=process.env.MONGO_URI;
   if (!MONGO_URI) {
     throw new Error('Please define the MONGODB_URI in the .env.local file');
   }

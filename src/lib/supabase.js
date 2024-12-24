30edb2f8eba50e5d35af4570cce86116e7e56e4c
souravscript <souravspace@Shivams-MacBook-Air.local>
2024-12-25 00:01:33 +0530

    vercel deployment build

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,5 +1,7 @@
 
 import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = 'https://iwlevkqihvpwhnsyuxel.supabase.co'
 
-//const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml3bGV2a3FpaHZwd2huc3l1eGVsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI0NDk4MjgsImV4cCI6MjA0ODAyNTgyOH0.3dB8M-Nv_yWTwujkJYo4qXd--lBzaVtJ_COSAzxz3Mw'
-export const supabase = createClient(process.env.NEXT_SUPABASE_URL, process.env.SUPABASE_ANON_KEY)
+const supabaseKey=process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+console.log(supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey)
